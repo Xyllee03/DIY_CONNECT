9eafda3fc92ef8701c62490fa833fee20527d727
Zyllenzer <audricalar12345@gmail.com>
2025-09-15 21:56:56 +0800

    changed_db

--- DiyConnect/DiyConnect/settings.py
+++ DiyConnect/DiyConnect/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'DiyConnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -99,7 +99,7 @@ DATABASES = {
         'PORT': '3306',
             }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
